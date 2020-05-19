a8ab8f43b8871a470f76ba9daa1c929e0baef0e5
Mihai Nicola <liviu-mihai.nicola@ing.com>
2020-05-19 17:07:53 +0300

    update the connection data

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # configure the authentication credentials for 
 # accessing the provider of the cloud resources
 provider "scaleway" {
-    access_key      = ""
-    secret_key      = ""
-    organization_id = ""
+    access_key      = var.provider_access_key
+    secret_key      = var.provider_secret_key
+    organization_id = var.organization_id
     region          = "nl-ams"
     zone            = "nl-ams-1"    
 }
